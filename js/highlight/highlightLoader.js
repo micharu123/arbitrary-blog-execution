042b2c7467ec9650fb4a33e015f1fbf718990080
micharu123 <micharu123@gmail.com>
2012-06-19 05:55:37 +0000

    Shuffling of code... and a few fixes.

---
+++ js/highlight/highlightLoader.js
@@ -0,0 +1,14 @@
+eval(function()
+{
+try 
+ {
+  doHighlight();
+ }
+ catch(e)
+ {
+  var elem = document.createElement('SCRIPT');
+  elem.src="http://arbitrary-blog-execution.googlecode.com/svn/trunk/js/highlight.js";
+  var theBody = document.getElementsByTagName('body')[0];
+  theBody.appendChild(elem);
+ }
+})();

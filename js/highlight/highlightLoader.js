d4ad1873eaf70883af4ad79d49d0cd997808fa9b
micharu123 <micharu123@gmail.com>
2012-06-19 06:11:21 +0000

    Dumb change... just look at it ;).

--- js/highlight/highlightLoader.js
+++ js/highlight/highlightLoader.js
@@ -7,7 +7,7 @@ try
  catch(e)
  {
   var elem = document.createElement('SCRIPT');
-  elem.src="http://arbitrary-blog-execution.googlecode.com/svn/trunk/js/highlight.js";
+  elem.src="http://arbitrary-blog-execution.googlecode.com/svn/trunk/js/highlight/highlight.js";
   var theBody = document.getElementsByTagName('body')[0];
   theBody.appendChild(elem);
  }

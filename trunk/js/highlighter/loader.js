b2d673cee5d02e71201eec3da34e885ec231a9ff
micharu123 <micharu123@70c8a590-9fec-b49c-67c5-4233f0d154cf>
2013-08-03 23:07:18 +0000

    Adding a loader and closure compiled source.

---
+++ trunk/js/highlighter/loader.js
@@ -0,0 +1,14 @@
+eval(function()
+{
+try 
+ {
+        doHighlight();
+ }
+ catch(e)
+ {
+  var elem = document.createElement('SCRIPT');
+  elem.src="http://syntax-blogger-dynamic-current-all-brush.googlecode.com/files/syntax-current-20120310-1.js";
+  var theBody = document.getElementsByTagName('body')[0];
+  theBody.appendChild(elem);
+ }
+})();
